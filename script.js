e7044fef5c69c02f133c7c5c8942485f68459699
Yoda <128441431+Fives117@users.noreply.github.com>
2025-03-10 19:53:52 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // JavaScript to add interactivity
 document.getElementById("clickMe").addEventListener("click", function () {
-    window.location.href = "page2.html"; // Change this to your actual next page
+    window.location.href = "https://github.com/Fives117/test/blob/main/page2.html"; // Change this to your actual next page
 });
