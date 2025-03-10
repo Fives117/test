48a6a457249cec29ebb1e64499c5d71d7ca716e7
Yoda <128441431+Fives117@users.noreply.github.com>
2025-03-10 20:14:01 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 document.getElementById("clickMe").addEventListener("click", function () {
     window.location.href = "page2.html"; // Change this to your actual next page
 });
+document.getElementById("clickMe2").addEventListener("click", function () {
+    window.location.href = "index.html"; // Change this to your actual next page
+});
