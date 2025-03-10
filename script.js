1406160754f21c0260dcb3afe208c67a2ab37398
Yoda <128441431+Fives117@users.noreply.github.com>
2025-03-10 19:51:50 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // JavaScript to add interactivity
-document.getElementById('clickMe').addEventListener('click', function() {
-    alert("You clicked the button!");
+document.getElementById("clickMe").addEventListener("click", function () {
+    window.location.href = "page2.html"; // Change this to your actual next page
 });
